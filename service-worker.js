d7a3619d1663282c05dcd5e3e94f54b27f1c4aaa
ServiceHermle <to.pospisil@centrum.cz>
2026-05-28 21:27:27 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VER = 'hdh-code-v3_1_46-help-news-language-fix';
+const CACHE_VER = 'hdh-code-v3_1_47-whatsnew-original-text';
 const CORE_ASSETS = [
   './index.html',
   './manifest.json',
